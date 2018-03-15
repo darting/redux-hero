986b46f7abc41917b6e004049303618358a53a9d
Valor <valorzhong@gmail.com>
2018-03-15 08:44:05 +0800

    update

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -3,31 +3,73 @@ import { AppContainer } from 'react-hot-loader';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
+
+export interface Position { x: number, y: number }
+export interface Stats { health: number, maxHealth: number }
+export interface Inventory { potions: number }
 
 export interface HeroState {
-    level: number
+    xp: number,
+    level: number,
+    position: Position,
+    stats: Stats,
+    inventory: Inventory
 }
+
+export const initialState: HeroState = {
+    xp: 0,
+    level: 1,
+    position: { x: 0, y: 0 },
+    stats: { health: 50, maxHealth: 50 },
+    inventory: { potions: 1 }
+};
 
 export enum ActionTypeKeys {
-    LEVEL_UP = "LEVEL_UP",
+    GAIN_XP = 'GAIN_XP',
+    LEVEL_UP = 'LEVEL_UP',
+    MOVE = 'MOVE',
+    DRINK_POTION = 'DRINK_POTION',
+    TAKE_DAMAGE = 'TAKE_DAMAGE',
 }
 
-export interface LevelUpAction {
-    type: ActionTypeKeys.LEVEL_UP
-}
+export interface GainXPAction { type: ActionTypeKeys.GAIN_XP, by: number }
+export interface LevelUpAction { type: ActionTypeKeys.LEVEL_UP }
+export interface MoveAction { type: ActionTypeKeys.MOVE, by: Position }
+export interface DrinkPotionAction { type: ActionTypeKeys.DRINK_POTION }
+export interface TakeDamageAction { type: ActionTypeKeys.TAKE_DAMAGE, by: number }
 
 
 export type ActionTypes =
+    | GainXPAction
     | LevelUpAction
+    | MoveAction
+    | DrinkPotionAction
+    | TakeDamageAction
 
 
-export function levelReducer(state: HeroState, action: ActionTypes) {
+export const levelReducer = (state = initialState.level, action: ActionTypes) => {
     switch (action.type) {
         case ActionTypeKeys.LEVEL_UP:
-            return { level: state.level + 1 };
+            return state + 1;
         default: return state;
     }
-} 
+}
 
-export const store = createStore(levelReducer);
+export const positionReducer = (state = initialState.position, action: ActionTypes) => {
+    switch (action.type) {
+        case ActionTypeKeys.MOVE:
+            let { x, y } = action.by;
+            x += state.x;
+            y += state.y;
+            return { x, y };
+        default: return state;
+    }
+}
+
+const reducer = combineReducers({
+    level: levelReducer,
+    position: positionReducer
+});
+
+export const store = createStore(reducer);
--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,13 @@ import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { App } from './App';
-
+import { store } from './Hero';
 
 
 const root = document.getElementById('root');
 const render = (Component: typeof App) =>
     ReactDOM.render(
-        <Provider>
+        <Provider store={store}>
             <AppContainer>
                 <Component />
             </AppContainer>
@@ -19,6 +19,16 @@ const render = (Component: typeof App) =>
         root
     );
 
-render(App);    
+render(App);
 
 if ((module as any).hot) (module as any).hot.accept('./App', () => render(App));
+
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch({ type: 'LEVEL_UP' });
+store.dispatch({ type: 'MOVE', by: { x: 10, y: 12 } });
+store.dispatch({ type: 'MOVE', by: { x: 20, y: 1 } });
+store.dispatch({ type: 'LEVEL_UP' });
